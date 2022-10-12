d0b81823a664fb622b2f1cf908cce62e753388fd
kdswenge <elogeswenge@gmail.com>
2022-10-12 02:13:00 +0200

    js

--- js/map.js
+++ js/map.js
@@ -1,21 +1,4 @@
 
-// var items, length, deg,z, move=0;
-// function rotate (direction) {
-//     move+=direction;
-//     for(var i=0;i<length;i++){
-//         items[i].style.transform="rotateY("+(deg*(i+move))+"deg) translateZ("+z+"px)";
-//     }
-// }
-// function load (){
-//     items= document.getElementsByClassName('carou');
-//     length= items.length;
-//     deg= 360/length;
-//     z=(items[0].offsetWidth/2)/Math.tan((deg/2)*(Math.PI/180));
-//    for(var i=0;i<length;i++){
-//        items[i].style.transform="rotateY("+(deg*i)+"deg) translateZ("+z+"px) ";
-//    }
-// }
-// window.addEventListener('load', load);
 var img= document.getElementsByClassName('img_slider');
 var etape=0;
 var nbr =img.length;
